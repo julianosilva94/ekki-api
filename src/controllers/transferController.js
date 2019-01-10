a3a9ef0bf8f66022ef37a322fd569c227cf8b9fd
Juliano Silva <juliano.silva2011@gmail.com>
2019-01-10 20:51:49 -0200

    implement transfer controller

--- src/controllers/transferController.js
+++ src/controllers/transferController.js
@@ -2,13 +2,18 @@ const express = require('express');
 
 const authMiddleware = require('../middlewares/auth');
 
+const Transfer = require('../models/transfer');
+const User = require('../models/user');
+
 const router = express.Router();
 
 router.use(authMiddleware);
 
 router.get('/', async (req, res) => {
   try {
-    return res.send({ user: req.userId });
+    const transfers = await Transfer.find({ $or: [{ from: req.userId }, { to: req.userId }] });
+
+    return res.send({ transfers });
   } catch (err) {
     return res.status(400).send({ error: 'Error' });
   }
@@ -16,7 +21,12 @@ router.get('/', async (req, res) => {
 
 router.get('/:transferId', async (req, res) => {
   try {
-    return res.send({ user: req.userId });
+    const transfer = await Transfer.findOne({
+      _id: req.params.transferId,
+      $and: [{ $or: [{ from: req.userId }, { to: req.userId }] }],
+    });
+
+    return res.send(transfer);
   } catch (err) {
     return res.status(400).send({ error: 'Error' });
   }
@@ -24,7 +34,35 @@ router.get('/:transferId', async (req, res) => {
 
 router.post('/', async (req, res) => {
   try {
-    return res.send({ user: req.userId });
+    // eslint-disable-next-line object-curly-newline
+    const { to, value, usedCreditCard } = req.body;
+
+    if (req.userId === to) {
+      return res.status(400).send({ error: 'Error' });
+    }
+
+    const userFrom = await User.findById(req.userId).select('+transfers +balance');
+    const userTo = await User.findById(to).select('+transfers +balance');
+
+    const transfer = await Transfer.create({
+      from: req.userId,
+      to,
+      value,
+      usedCreditCard,
+    });
+
+    // eslint-disable-next-line no-underscore-dangle
+    userFrom.transfers.push(transfer);
+    userFrom.balance -= value;
+
+    // eslint-disable-next-line no-underscore-dangle
+    userTo.transfers.push(transfer);
+    userTo.balance += value;
+
+    await userFrom.save();
+    await userTo.save();
+
+    return res.send({ transfer, balance: userFrom.balance });
   } catch (err) {
     return res.status(400).send({ error: 'Error' });
   }

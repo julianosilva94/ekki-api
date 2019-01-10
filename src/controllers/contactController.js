14f282e37beea815c9f8ad79f8196959f3f58220
Juliano Silva <juliano.silva2011@gmail.com>
2019-01-10 13:47:20 -0200

    add controllers

---
+++ src/controllers/contactController.js
@@ -0,0 +1,49 @@
+const express = require('express');
+
+const authMiddleware = require('../middlewares/auth');
+
+const router = express.Router();
+
+router.use(authMiddleware);
+
+router.get('/', async (req, res) => {
+  try {
+    return res.send({ user: req.userId });
+  } catch (err) {
+    return res.status(400).send({ error: 'Error' });
+  }
+});
+
+router.get('/:contactId', async (req, res) => {
+  try {
+    return res.send({ user: req.userId });
+  } catch (err) {
+    return res.status(400).send({ error: 'Error' });
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    return res.send({ user: req.userId });
+  } catch (err) {
+    return res.status(400).send({ error: 'Error' });
+  }
+});
+
+router.put('/:contactId', async (req, res) => {
+  try {
+    return res.send({ user: req.userId });
+  } catch (err) {
+    return res.status(400).send({ error: 'Error' });
+  }
+});
+
+router.delete('/:contactId', async (req, res) => {
+  try {
+    return res.send({ user: req.userId });
+  } catch (err) {
+    return res.status(400).send({ error: 'Error' });
+  }
+});
+
+module.exports = app => app.use('/contacts', router);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const helmet = require('helmet');
 
 const authController = require('./controllers/authController');
+const creditCardController = require('./controllers/creditCardController');
+const contactController = require('./controllers/contactController');
+const transferController = require('./controllers/transferController');
 
 require('dotenv').config();
 
@@ -20,5 +23,8 @@ app.get('/', (req, res) => {
 });
 
 authController(app);
+creditCardController(app);
+contactController(app);
+transferController(app);
 
 app.listen(PORT, HOST);

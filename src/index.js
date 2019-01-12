70b29680a82bd3c597977b74421a0db6abc8c719
Juliano Silva <juliano.silva2011@gmail.com>
2019-01-11 22:14:26 -0200

    add cors config

--- src/index.js
+++ src/index.js
@@ -18,6 +18,12 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('API Working');
 });

21bcff2f45e60bb807607147b2c955e963f0f50f
Juliano Silva <juliano.silva2011@gmail.com>
2019-01-12 21:39:58 -0200

    change cors config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
+const cors = require('cors');
 
 const authController = require('./controllers/authController');
 const creditCardController = require('./controllers/creditCardController');
@@ -18,11 +19,7 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('API Working');

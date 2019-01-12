93c0a57ce525cc2d5404e5bede2d3bcef21fa006
Juliano Silva <juliano.silva2011@gmail.com>
2019-01-12 21:43:28 -0200

    add some validations on transfer controller

--- src/controllers/transferController.js
+++ src/controllers/transferController.js
@@ -11,7 +11,9 @@ router.use(authMiddleware);
 
 router.get('/', async (req, res) => {
   try {
-    const transfers = await Transfer.find({ $or: [{ from: req.userId }, { to: req.userId }] });
+    const transfers = await Transfer.find({ $or: [{ from: req.userId }, { to: req.userId }] })
+      .populate('from', 'name')
+      .populate('to', 'name');
 
     return res.send({ transfers });
   } catch (err) {
@@ -36,28 +38,34 @@ router.post('/', async (req, res) => {
   try {
     // eslint-disable-next-line object-curly-newline
     const { to, value, usedCreditCard } = req.body;
+    const convertedValue = parseInt(value, 10);
 
-    if (req.userId === to) {
+    if (value <= 0 || req.userId === to) {
       return res.status(400).send({ error: 'Error' });
     }
 
     const userFrom = await User.findById(req.userId).select('+transfers +balance');
+
+    if (userFrom.balance < convertedValue) {
+      return res.status(400).send({ error: 'Error' });
+    }
+
     const userTo = await User.findById(to).select('+transfers +balance');
 
     const transfer = await Transfer.create({
       from: req.userId,
       to,
-      value,
+      convertedValue,
       usedCreditCard,
     });
 
     // eslint-disable-next-line no-underscore-dangle
     userFrom.transfers.push(transfer);
-    userFrom.balance -= value;
+    userFrom.balance -= convertedValue;
 
     // eslint-disable-next-line no-underscore-dangle
     userTo.transfers.push(transfer);
-    userTo.balance += value;
+    userTo.balance += convertedValue;
 
     await userFrom.save();
     await userTo.save();
